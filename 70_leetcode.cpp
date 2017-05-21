970bea2448c9669afcd5e2d8215476c0414c55af
Aalekh Nigam <aalekh.nigam@gmail.com>
2017-05-22 04:06:06 +0530

    Create 70_leetcode.cpp

---
+++ 70_leetcode.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fibo(int n){
+        if(n <= 1) return 1;
+        return fibo(n-1) + fibo(n-2);
+    }
+    
+    int climbStairs(int n) {
+        // n = n+1;
+        return fibo(n);
+    }
+};
